a1c7cd8708f8811a79abba457bcc0228716c0ce4
FangYu199 <talentyupush@163.com>
2024-06-24 14:21:18 +0800

    Add files via upload

---
+++ public/static/js/script.js
@@ -0,0 +1,28 @@
+$('.k_nav_btn').click(function() {
+$('.k_nav').slideToggle(300);
+});
+
+$('.k-dg').matchHeight();
+$('.radiohe').click(function() {
+    if ($(this).val() == 1){
+        $(this).removeClass('active').val('')
+    }else {
+        $(this).addClass('active').val(1)
+    }
+    f();
+});
+$('.radiohes').click(function() {
+    if ($(this).val() == 1){
+        $(this).removeClass('active').val('')
+    }else {
+        $(this).addClass('active').val(1)
+    }
+    f();
+});
+function f() {
+    if ($('.radiohe').val() == 1 && $('.radiohes').val() == 1){
+        $('.fromBtn').addClass('active')
+    }else {
+        $('.fromBtn').removeClass('active')
+    }
+}
